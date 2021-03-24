cd2883622dcf09b351f37e9575ebe2f90f085d36
Dennis Fazio <fazioferreira@gmail.com>
2021-03-24 06:22:00 -0300

    Criado os steps WebService

--- features/specifications/validacao_api_users.feature
+++ features/specifications/validacao_api_users.feature
@@ -12,8 +12,8 @@
 
       @webservice @get_posts
       Cenario: validar se a API está retornando o código 200 e os dados corretos
-      Quando quando realizado um get para api users sem passar parametros
-      E realizo um get para o endpoint posts com o primeiro ID User retornado do get anterior
+      Dado quando realizado um get para api users sem passar parametros
+      Quando realizo um get para o endpoint posts com o primeiro ID User retornado do get anterior
       Então devo validar se o statuscode retornou 200
       E validar se todos os posts retornados são do usuário pesquisado
 
---
+++ features/step_definitions/validacao_api_users_step.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+######  DADO  ######
+Dado("quando realizado um get para api users sem passar parametros") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+######  QUANDO  ######
+Quando("quando realizado um get passando um nome aleatório do arquivo de users") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("realizo um get para o endpoint posts com o primeiro ID User retornado do get anterior") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+######  ENTÃO  ######
+
+Então("devo validar se o statuscode retornou {int}") do |int|
+  # Então('devo validar se o statuscode retornou {float}') do |float|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("validar se todos elementos do array data possuem o nome pesquisado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("validar se todos os posts retornados são do usuário pesquisado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
