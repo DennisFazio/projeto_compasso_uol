c78480848a42c447f5c21dd0cd2b32cefdb08476
Dennis Fazio <fazioferreira@gmail.com>
2021-03-23 21:22:33 -0300

    Step do Web Front Criadas

--- features/step_definitions/selecao_e_compra_de_produto_step.rb
+++ features/step_definitions/selecao_e_compra_de_produto_step.rb
@@ -1,7 +1,34 @@
 # frozen_string_literal: true
 
 ######  DADO  ######
+Dado("que estou na home do site automationpractice") do
+  home.load
+  binding.pry
+end
 
 ######  QUANDO  ######
+Quando("realizo a busca pelo produto {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("seleciono para visualizar mais detalhes do produto") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("adiciono o primeiro produto ao carrinho") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
 ######  ENTÃO  ######
+
+Então("devo visualizar o detalhe do produto selecionado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("devo visualizar o produto no carrinho") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("finalizar o processo de compra do produto selecionado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
