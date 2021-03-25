8f51ed5c63e410535eb5ceee020804db846aff9e
Dennis Fazio <fazioferreira@gmail.com>
2021-03-25 09:42:34 -0300

    ajuste env

--- features/support/env.rb
+++ features/support/env.rb
@@ -22,7 +22,7 @@ API = YAML.load_file(Dir.pwd + "/features/support/generators/fixtures/ambientes_
 
 ##### CONFIG CAPYBARA #####
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome #:chrome # :selenium_chrome_headless ou :selenium_chrome
+  config.default_driver = :selenium_chrome # :selenium_chrome_headless ou :selenium_chrome
   config.javascript_driver = :selenium_chrome
   config.app_host = CONFIG[AMBIENTE]
   config.default_max_wait_time = 15
